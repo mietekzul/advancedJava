29b8ca6f0153acb9453c48c1ad6260c6a6bd511a
mietekzul <noname_18@wp.pl>
2016-10-10 23:20:52 +0200

    Exceptions.

---
+++ src/main/java/exceptions/MyException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class MyException extends Exception {
+
+	public MyException(String message) {
+		super(message);
+	}
+
+	public MyException() {
+		this("default message");
+	}
+}
