a580a7c6ee046fcb78b71734f9430a1045e84d97
mietekzul <noname_18@wp.pl>
2016-10-10 22:48:05 +0200

    Added more classes.

--- src/main/java/interfaces/Task.java
+++ src/main/java/interfaces/Task.java
@@ -4,106 +4,106 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Task {
-	private int id;
-	private String name;
-	private LocalDate startDate = LocalDate.now();
-	private LocalDate endDate = LocalDate.now();
-	private int priority;
-	private boolean completed;
+    private int id;
+    private String name;
+    private LocalDate startDate = LocalDate.now();
+    private LocalDate endDate = LocalDate.now();
+    private int priority;
+    private boolean completed;
 
-	public Task() {
-	}
+    public Task() {
+    }
 
-	public Task(int id, String name, int priority) {
-		this(id, name, LocalDate.now(), LocalDate.now(), priority, false);
-	}
+    public Task(int id, String name, int priority) {
+        this(id, name, LocalDate.now(), LocalDate.now(), priority, false);
+    }
 
-	public Task(int id, String name, LocalDate startDate, LocalDate endDate, int priority, boolean completed) {
-		this.id = id;
-		this.name = name;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.priority = priority;
-		this.completed = completed;
-	}
+    public Task(int id, String name, LocalDate startDate, LocalDate endDate, int priority, boolean completed) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priority = priority;
+        this.completed = completed;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public LocalDate getStartDate() {
-		return startDate;
-	}
+    public LocalDate getStartDate() {
+        return startDate;
+    }
 
-	public void setStartDate(LocalDate startDate) {
-		this.startDate = startDate;
-	}
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
 
-	public LocalDate getEndDate() {
-		return endDate;
-	}
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 
-	public void setEndDate(LocalDate endDate) {
-		this.endDate = endDate;
-	}
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 
-	public int getPriority() {
-		return priority;
-	}
+    public int getPriority() {
+        return priority;
+    }
 
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 
-	public boolean isCompleted() {
-		return completed;
-	}
+    public boolean isCompleted() {
+        return completed;
+    }
 
-	public void setCompleted(boolean completed) {
-		this.completed = completed;
-	}
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("Task{");
-		sb.append("id=").append(id);
-		sb.append(", name='").append(name).append('\'');
-		sb.append(", startDate=").append(startDate);
-		sb.append(", endDate=").append(endDate);
-		sb.append(", priority=").append(priority);
-		sb.append(", completed=").append(completed);
-		sb.append('}');
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Task{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", startDate=").append(startDate);
+        sb.append(", endDate=").append(endDate);
+        sb.append(", priority=").append(priority);
+        sb.append(", completed=").append(completed);
+        sb.append('}');
+        return sb.toString();
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Task task = (Task) o;
-		return id == task.id &&
-				priority == task.priority &&
-				completed == task.completed &&
-				Objects.equals(name, task.name) &&
-				Objects.equals(startDate, task.startDate) &&
-				Objects.equals(endDate, task.endDate);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id &&
+                priority == task.priority &&
+                completed == task.completed &&
+                Objects.equals(name, task.name) &&
+                Objects.equals(startDate, task.startDate) &&
+                Objects.equals(endDate, task.endDate);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name, startDate, endDate, priority, completed);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, startDate, endDate, priority, completed);
+    }
 
 }
--- src/main/java/oop/Employee.java
+++ src/main/java/oop/Employee.java
@@ -5,58 +5,58 @@ import java.util.Objects;
 
 public abstract class Employee {
 
-	public static final String DEFAULT_NAME = "UNKNOWN";
-	private static int nextId;
+    public static final String DEFAULT_NAME = "UNKNOWN";
+    private static int nextId;
 
-	private Integer id;
-	private String name;
-	private LocalDate hireDate;
+    private Integer id;
+    private String name;
+    private LocalDate hireDate;
 
-	public Employee() {
-		this(DEFAULT_NAME);
-	}
+    public Employee() {
+        this(DEFAULT_NAME);
+    }
 
-	public Employee(String name) {
-		id = nextId++;
-		this.name = name;
-		hireDate = LocalDate.now();
-	}
+    public Employee(String name) {
+        id = nextId++;
+        this.name = name;
+        hireDate = LocalDate.now();
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	private void setId(Integer id) {
-		this.id = id;
-	}
+    private void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public abstract double getPay();
+    public abstract double getPay();
 
-	@Override
-	public String toString() {
-		return String.format("Employee{id=%d, name='%s', hireDate=%s}", id, name, hireDate);
-	}
+    @Override
+    public String toString() {
+        return String.format("Employee{id=%d, name='%s', hireDate=%s}", id, name, hireDate);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Employee employee = (Employee) o;
-		return Objects.equals(id, employee.id) &&
-				Objects.equals(name, employee.name) &&
-				Objects.equals(hireDate, employee.hireDate);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(hireDate, employee.hireDate);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name, hireDate);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, hireDate);
+    }
 }
